e34e2ddeb545bb353cbe63f083e57fcef49be4e3
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-05-02 10:23:41 -0400

    Add settings with system tray function

---
+++ src/main/kotlin/util/Settings.kt
@@ -0,0 +1,13 @@
+package util
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class Settings {
+    var isTrayEnabled by mutableStateOf(true)
+        private set
+    fun toggleTray() {
+        isTrayEnabled = !isTrayEnabled
+    }
+}
