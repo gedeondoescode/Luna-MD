2ea8d294c52ed30031ad5c4c4c0185ccbde7a9ee
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-05-02 20:42:52 -0400

    refactoring the project for strictly text

--- src/main/kotlin/os/Resources.kt
+++
@@ -1,9 +0,0 @@
-package os
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-
-@Composable
-expect fun Font(name: String, res: String, weight: FontWeight, style: FontStyle ): Font //This is throwing an error: No declaration for mainKt
