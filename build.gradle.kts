71fec0d50973495c0877a4e43e9972c1bdd38124
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-05-06 20:37:57 -0400

    update compose to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.6.1")
 }
 
 compose.desktop {
